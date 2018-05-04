18061038940a54d5955d8f5d63b7311cd9d92bb9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-04 17:14:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,6 +25,8 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   # add 2 muskets to the shipping_manifest hash below
 
 
